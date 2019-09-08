97652040ed92382b76d4ff1c74a38f2b9f70e339
Rakibul Hasan <30563711+voomoo@users.noreply.github.com>
2019-09-08 14:28:06 +0600

    added new server

--- test-script.js
+++ test-script.js
@@ -1,82 +1,85 @@
-const newLocal = '.summary';
-window.addEventListener('load', () => {
+window.addEventListener('load', () =>{
     let lat;
     let long;
 
-    function time() {
+    function time(){
         const refresh = 1000;
-
-        function currentTime() {
+        
+        function currentTime(){
             today = new Date();
             time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-            document.querySelector('.time').innerHTML = time;
+            document.querySelector('.currentTime').innerHTML=time;
             setTimeout(currentTime, refresh);
         }
-
+        
         currentTime();
     }
 
-    function darksky() {
-        document.querySelector('.server').innerHTML = "darksky";
+    document.querySelector('.currentTime').innerHTML=time();
+
+    function darkSky(){
+        document.querySelector(".darkSky").classList.toggle('active');
+        document.querySelector(".openWeather").classList.toggle('active');
 
         var proxy = "https://cors-anywhere.herokuapp.com/";
-        var api = `${proxy}https://api.darksky.net/forecast/8763c747e005243c042e988b881328f6/${lat},${long}`;
+        var api = `${proxy}https://api.darksky.net/forecast/240a0e65cbe0b24e907f6b1eb0655235/${lat},${long}`;
         //calling api
         fetch(api)
-            .then(response => {
-                //parsing string to json
-                return response.json();
-            })
+        .then(response =>{
+            //parsing string to json
+            return response.json();
+        })
 
-            .then(data => {
-                var location = data.timezone;
-                var temp = data.currently.temperature;
-                temp = (temp - 32) * 5 / 9;
-                temp = Number(temp).toFixed(2);
-                var summary = data.currently.summary;
-                var today = new Date();
-                //displaying result in window
-                document.querySelector('.location').innerHTML = location;
-                document.querySelector('.temp').innerHTML = temp + " C";
-                document.querySelector('.time').innerHTML = time();
-                document.querySelector(newLocal).innerHTML = summary;
-            })
+        .then(data =>{
+            console.log(data);
+            var location = data.timezone;
+            var temp = data.currently.temperature;
+            temp = (temp - 32)*5/9;
+            temp = Number(temp).toFixed(2);
+            var summary = data.currently.summary;
+            var today = new Date();
+            //displaying result in window
+            document.querySelector('.currentLocation').innerHTML=location;
+            document.querySelector('.currentTemp').innerHTML=temp + " C";
+            document.querySelector('.summary').innerHTML=summary;
+        })
     }
 
-    function openweather() {
-        document.querySelector('.server').innerHTML = "openweather";
+    function openWeather(){
+        document.querySelector(".darkSky").classList.toggle('active');
+        document.querySelector(".openWeather").classList.toggle('active');
 
         const proxy = "https://cors-anywhere.herokuapp.com/";
-        const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&APPID=b33d3c6dbe5a3a4cde65619771f5bfdb`;
+            const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&APPID=99b452ff7f185125eb45f460029fd177`;
         //calling api
         fetch(api)
-            .then(response => {
-                //parsing string to json
-                return response.json();
-            })
+        .then(response =>{
+            //parsing string to json
+            return response.json();
+        })
 
-            .then(data => {
-                let location = data.name;
-                let temp = data.main.temp;
-                let summary = data.weather[0].description;
-                //displaying result in window
-                document.querySelector('.location').innerHTML = location;
-                document.querySelector('.temp').innerHTML = temp + " C";
-                document.querySelector('.time').innerHTML = time();
-                document.querySelector('.summary').innerHTML = summary;
-            })
+        .then(data =>{
+            console.log(data);
+            let location = data.name;
+            let temp = data.main.temp;
+            let summary = data.weather[0].description;
+            //displaying result in window
+            document.querySelector('.currentLocation').innerHTML=location;
+            document.querySelector('.currentTemp').innerHTML=temp + " C";
+            document.querySelector('.summary').innerHTML=summary;
+        })       
     }
     //getting position
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position =>{
             //getting position
-            lat = position.coords.latitude;
-            long = position.coords.longitude;
+            lat=position.coords.latitude;
+            long=position.coords.longitude;
         })
         //calling openWeather as default server
-        openweather();
+        openWeather();
     }
     //on demand server
-    document.querySelector(".darksky").addEventListener('click', darksky);
-    document.querySelector(".openweather").addEventListener('click', openweather);
+    document.querySelector(".darkSky").addEventListener('click', darkSky);
+    document.querySelector(".openWeather").addEventListener('click', openWeather);
 })
